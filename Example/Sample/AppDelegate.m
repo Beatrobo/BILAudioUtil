5621d0ce2aee9ff8a1e19fb3ab5f0ac8c1e8eaf6
Yusuke SUGAMIYA <yusuke.dnpp@gmail.com>
2015-02-27 20:23:11 +0900

    デバッグ用 UI 入れ込んだ

--- Example/Sample/AppDelegate.m
+++ Example/Sample/AppDelegate.m
@@ -11,8 +11,7 @@
 
 - (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
 {
-    UIViewController* vc = [[BILAudioVolumeManagerTestViewController alloc] initWithNibName:nil bundle:nil];
-    self.window.rootViewController = vc;
+    self.window.rootViewController = [[BILAudioVolumeManagerTestViewController alloc] initWithNibName:nil bundle:nil];
     return YES;
 }
 

50b36975afbd44eecc902bbec53da0e06635786e
Yusuke SUGAMIYA <yusuke.dnpp@gmail.com>
2015-02-27 16:50:04 +0900

    add Example App

---
+++ Example/SampleTests/BILAudioUtilSampleTests.m
@@ -0,0 +1,33 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+
+@interface BILAudioUtilSampleTests : XCTestCase
+
+@end
+
+
+@implementation BILAudioUtilSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample
+{
+    [self measureBlock:^{
+    }];
+}
+
+@end

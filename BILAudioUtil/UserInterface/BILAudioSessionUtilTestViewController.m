dafd79e2266fdf409134db326744b841a70be907
Yusuke SUGAMIYA <yusuke.dnpp@gmail.com>
2015-03-05 21:31:39 +0900

    remove sample UI

--- BILAudioUtil/UserInterface/BILAudioSessionUtilTestViewController.m
+++ BILAudioUtil/UserInterface/BILAudioSessionUtilTestViewController.m
@@ -1,44 +1,15 @@
 #import "BILAudioSessionUtilTestViewController.h"
 #import "BILAudioSessionUtil.h"
-#if !(TARGET_IPHONE_SIMULATOR)
-#import "DPSystemVolumeController.h"
-#endif
 
 
 @interface BILAudioSessionUtilTestViewController ()
-#if !(TARGET_IPHONE_SIMULATOR)
-<DPSystemVolumeControllerObserving>
-#endif
-@property (nonatomic, weak) IBOutlet UISlider* ringtoneVolumeSlider;
-@property (nonatomic, weak) IBOutlet UILabel*  ringtoneVolumeLabel;
-@property (nonatomic, weak) IBOutlet UISlider* audioVideoVolumeSlider;
-@property (nonatomic, weak) IBOutlet UILabel*  audioVideoVolumeLabel;
+
 @end
 
 
 @implementation BILAudioSessionUtilTestViewController
 
 #pragma mark - UIViewController
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-    #if !(TARGET_IPHONE_SIMULATOR)
-    [[DPSystemVolumeController sharedController] addSystemVolumeControllerObserver:self];
-    
-    {
-        float volume = [DPSystemVolumeController sharedController].volumeForRingtone;
-        self.ringtoneVolumeSlider.value = volume;
-        self.ringtoneVolumeLabel.text = [NSString stringWithFormat:@"%.2f", volume];
-    }
-    {
-        float volume = [DPSystemVolumeController sharedController].volumeForAudioVideo;
-        self.audioVideoVolumeSlider.value = volume;
-        self.audioVideoVolumeLabel.text = [NSString stringWithFormat:@"%.2f", volume];
-    }
-    #endif
-}
 
 - (void)viewDidAppear:(BOOL)animated
 {
@@ -137,45 +108,5 @@
     }
     #endif
 }
-
-- (IBAction)valueChangedRingtoneVolumeSlider:(UISlider*)sender
-{
-    #if !(TARGET_IPHONE_SIMULATOR)
-    [DPSystemVolumeController sharedController].volumeForRingtone = sender.value;
-    #endif
-}
-
-- (IBAction)valueChangedAudioVideoVolumeSlider:(UISlider*)sender
-{
-    #if !(TARGET_IPHONE_SIMULATOR)
-    [DPSystemVolumeController sharedController].volumeForAudioVideo = sender.value;
-    #endif
-}
-
-#if !(TARGET_IPHONE_SIMULATOR)
-
-#pragma mark - BILAudioVolumeManagerObserving
-
-- (void)systemVolumeController:(DPSystemVolumeController*)audioVolumeManager
-               didChangeVolume:(float)volume
-               isExplictChange:(BOOL)isExplictChange
-                 audioCategory:(id)audioCategory
-{
-    if ([audioCategory isKindOfClass:[NSString class]] && [audioCategory isEqualToString:@"Ringtone"]) {
-        if (self.ringtoneVolumeSlider.isTracking == NO) {
-            self.ringtoneVolumeSlider.value = volume;
-        }
-        self.ringtoneVolumeLabel.text = [NSString stringWithFormat:@"%.2f", volume];
-    }
-    else if ([audioCategory isKindOfClass:[NSString class]] && [audioCategory isEqualToString:@"Audio/Video"]) {
-        if (self.audioVideoVolumeSlider.isTracking == NO) {
-            self.audioVideoVolumeSlider.value = volume;
-        }
-        self.audioVideoVolumeLabel.text = [NSString stringWithFormat:@"%.2f", volume];
-    }
-
-}
-
-#endif
 
 @end
